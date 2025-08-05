13c996df537c42aa7450f0b5a976f1f3e46d8cf1
NiclasJe <78955217+NiclasJe@users.noreply.github.com>
2025-08-06 00:23:31 +0200

    Set base to /niclas-portfolio/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/niclas-portfolio/' : '/',
+  base: '/niclas-portfolio/',
   server: {
     host: "::",
     port: 8080,
